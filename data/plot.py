34e4216bb8d37e049ee2f45abd1e5eaca418b156
Jure Šorn <sornjure@gmail.com>
2018-04-05 14:11:32 +0200

    Working on plot

--- data/plot.py
+++ data/plot.py
@@ -8,29 +8,91 @@ import re
 import matplotlib.pyplot as plt
 
 
+DEBUG = False
+
 def main():
     songs = read_json_file('wiki_data.json')
-    years = get_years(songs)
-    generate_release_dates_chart(years)
+
+    generate_plot(songs, 'released', get_year, 'years')
+    generate_plot(songs, 'released', get_month, 'months')
+    generate_plot(songs, 'length', get_minutes, 'minutes')
+
+    #
+    # years = parse_releases(songs, 'released', get_year)
+    # generate_release_dates_chart(years)
+    # months = parse_releases(songs, 'released', get_month)
+    # generate_release_dates_chart(months)
+    # lengths = parse_releases(songs, 'length', get_minutes)
+    # generate_release_dates_chart(lengths, 'minutes')
 
 
-def get_years(songs):
+def generate_plot(songs, key, parser, xlabel):
+    values = parse_releases(songs, key, parser)
+    generate_release_dates_chart(values, filename=xlabel)
+
+
+def parse_releases(songs, key, parser):
     out = []
     for song in songs.values():
-        if 'released' not in song:
+        if key not in song:
             continue
-        release = song['released']
+        release = song[key]
         if len(release) < 1:
             continue
         if isinstance(release, list):
             release = release[0]
-        year = re.search('\d{4}', release)
-        if year:
-            out.append(int(year.group()))
+        value = parser(release)
+        if value:
+            out.append(int(value))
+        elif DEBUG:
+            print(release)
     return sorted(out)
 
 
-def generate_release_dates_chart(listOfYears):
+def get_minutes(length):
+    if ':' in length:
+        return length.split(':')[0]
+
+
+def get_year(release):
+    year = re.search('\d{4}', release)
+    if year:
+        return year.group()
+
+
+MONTHS = {'january': 1,
+          'february': 2,
+          'march': 3,
+          'april': 4,
+          'may' : 5,
+          'june': 6,
+          'july': 7,
+          'august': 8,
+          'september': 9,
+          'october': 10,
+          'november': 11,
+          'december': 12}
+
+
+def get_month(release):
+    if re.search('[a-zA-Z]', release):
+        month = re.search('january|february|march|april|may|june|july|august|september|october|november|december', release, flags=re.IGNORECASE)
+        if month:
+            month = month.group()
+            return MONTHS[month.lower()]
+    return get_numeric_month(release)
+
+
+
+def get_numeric_month(release):
+    if re.search('\.', release):
+        tokens = release.split('.')
+        digit_month = re.match('\d+', tokens[1])
+        if digit_month:
+            return digit_month.group()
+
+
+def generate_release_dates_chart(listOfYears, filename=None):
     albumsPerYear = getAlbumsPerYear(listOfYears)
     yearRange = getYearRange(listOfYears)
 
@@ -45,9 +107,15 @@ def generate_release_dates_chart(listOfYears):
     x_ticks = [listOfYears[0]-1] + yearRange + [listOfYears[-1]+1]
     x_ticks = [t for t in x_ticks if t%2==0]
     plt.xticks(x_ticks)
+    if filename:
+        plt.xlabel(filename.capitalize())
     plt.bar(x, y, color="blue")
 
-    plt.show()
+    if not filename:
+        plt.show()
+    else:
+        plt.savefig(filename)
+    plt.close()
 
 
 def getAlbumsPerYear(listOfYears):
